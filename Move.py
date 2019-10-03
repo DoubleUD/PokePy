3e0fa547d85d9d9beb9cda9023c24e86c9e6b232
rbuckley25 <44620278+rbuckley25@users.noreply.github.com>
2019-10-03 22:42:23 +0100

    Create Move.py

---
+++ Move.py
@@ -0,0 +1,12 @@
+
+#methods to be added
+
+class Move:
+
+    def __init__(self,name,type,power,accuracy,category, effect):
+        self.name = name
+        self.type = type
+        self.power = power
+        self.accuracy = accuracy
+        self.cat = category
+        self.effect = effect
