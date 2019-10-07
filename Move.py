de25f417e65194a21eb19081c3b0be816a1a5c06
DoubleUD <31520058+DoubleUD@users.noreply.github.com>
2019-10-07 01:29:26 +0100

    Update Move.py

--- Move.py
+++ Move.py
@@ -3,11 +3,11 @@
 
 class Move:
 
-    def __init__(self, name, type, power, accuracy, category, effect, target = opponent):
+    def __init__(self,name,type,power,accuracy,category, pp, effect = None):
         self.name = name
         self.type = type
         self.power = power
         self.accuracy = accuracy
         self.cat = category
+        self.pp = pp
         self.effect = effect
-        self.target = target
