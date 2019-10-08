0b1401e6dd5d9d5a2ff61952cde7ab068a8e3553
DoubleUD <31520058+DoubleUD@users.noreply.github.com>
2019-10-09 00:41:35 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import Pokemon
+import Data
 
-Bulbasaur = Pokemon.Species(1,"Bulbasaur", [45, 49, 49, 65, 65, 45], 12, 3, "Ivysaur",[],[],"g")
-A1 = Pokemon.Mon(Bulbasaur, [31,31,31,31,31,31], [0,0,0,0,0,0], 1, [0,1,2,3,4], "Bold", 300, 0, 1, False, 8, "Budley", 4466)
+Bulbasaur = Pokemon.Species(1,"Bulbasaur", [45, 49, 49, 65, 65, 45], 12, 3, "Ivysaur",[],[],3)
+A1 = Pokemon.Mon(Bulbasaur, [31,31,31,31,31,31], [0,152,0,0,100,252], 1, [0,1,2,3,4], "Bold", 1, 0, 1, False, 8, 4466)
 print(A1.hp)
-print(Pokemon.typechart)
+print(A1.name)
 def pt():
-    print(5)
+    print(A1.exp)
+    A1.exp += 20000
+    A1.checklvlup()
+    print(A1.lvl)
+pt()
+print(A1.hp, A1.attack, A1.defense, A1.spattack, A1.spdefense, A1.speed)
 
 
-Tackle = Pokemon.Move("Tackle", 0, 40, 100, 0, pt())
 
-Tackle.effect
